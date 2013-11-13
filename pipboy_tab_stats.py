a8a3e0d0f351880d1e192279c779c257381a3601
Neal D Corbett <bitbucket@obliviongizmo.com>
2013-11-13 02:02:37 +0000

    Modified pipboy_tab_stats.py

--- pipboy_tab_stats.py
+++ pipboy_tab_stats.py
@@ -1,3 +1,7 @@
+# RasPipBoy: A Pip-Boy 3000 implementation for Raspberry Pi
+#	Neal D Corbett, 2013
+# Class for 'Stats' tab
+
 import pygame, os, time, random, math, string, numpy
 from pygame.locals import *
 
