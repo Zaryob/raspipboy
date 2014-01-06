bc3c2ab19895938b0db3d31f8c8a933e7e43acd0
Neal D Corbett <bitbucket@obliviongizmo.com>
2014-01-06 01:50:22 +0000

    Modified pipboy_tab_stats.py

--- pipboy_tab_stats.py
+++ pipboy_tab_stats.py
@@ -92,7 +92,6 @@ class Tab_Stats:
 						elif (self.frameNum == 15):
 							self.frameNum = -1;
 						self.frameNum += 1;
-						print self.frameNum;
 				
 				elif (self.name == 'WAN'):	# Show WiFi signal-level
 					newVal = 0
