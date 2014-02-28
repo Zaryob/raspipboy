e50b423170becd613e6329a8970ef0597bd80ad3
Neal D Corbett <bitbucket@obliviongizmo.com>
2014-02-28 23:09:12 +0000

    Modified config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@
 # Device options 
 #  (These will be automatically be set to 'False' if unavailable)
 USE_INTERNET = True		# Download map/place data via internet connection
-USE_GPS = False			# Use GPS module, accessed via GPSD daemon
+USE_GPS = True			# Use GPS module, accessed via GPSD daemon
 USE_SOUND = True		# Play sounds via RasPi's current sound-source
-USE_CAMERA = False		# Use RasPi camera-module as V.A.T.S
+USE_CAMERA = True		# Use RasPi camera-module as V.A.T.S
 USE_SERIAL = True		# Communicate with custom serial-port controller
 
 QUICKLOAD = False		# If true, commandline-startup bits aren't rendered
